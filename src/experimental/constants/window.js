0605c05d0c6a6aca92d5db04d44a6ccaf0e69c96
Adam Procter <adamprocter@mac.com>
2023-09-28 19:04:35 +0100

    removed files

--- src/experimental/constants/window.js
+++
@@ -1,5 +0,0 @@
-import { isClient } from '@/experimental/utils/helpers'
-
-export const width = isClient ? window.innerWidth : 1000
-
-export const height = isClient ? window.innerHeight : 1000
--- src/experimental/icons/library/minus.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <rect x="13" y="24" width="23" height="2" />
-</template>
